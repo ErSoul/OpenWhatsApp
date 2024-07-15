cc401aecc68f2e3a595850ae9c59aa6b84dc2f7a
David Colmenares <ersoul@sdf.org>
2024-07-15 01:40:55 -0400

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const URL = (phone: string, text: string) => `https://api.whatsapp.com/send?phone=${phone}&text=${text}`;
+
+const App = () => {
+
+  const [phone, setPhone] = useState<string>('');
+  const [text, setText] = useState<string>();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const url = URL(phone, text? text : 'hola');
+    // window.open(url, '_blank');
+    window.location.href = url;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <img src='img/banner.jpg' id='logo' alt='logo'></img>
+        <h1>Open in WhatsApp</h1>
+        <p>Start a chat with any phone number you want<br/>without the need to add it as a contact!</p>
       </header>
-    </div>
+      <article>
+        <form onSubmit={handleSubmit}>
+          <label>Phone number:</label>
+          <input type="text" required value={phone} placeholder='e.g. +1234567890' pattern='\+[0-9]{7,}' onChange={(e) => setPhone(e.target.value)} />
+          <label>Message:</label>
+          <input type="text" placeholder='(optional)' value={text} onChange={(e) => setText(e.target.value)} />
+          <button type="submit" className='btn'>Submit</button>
+        </form>
+      </article>
+    </>
   );
 }
 
